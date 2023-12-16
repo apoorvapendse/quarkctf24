73d24cc852b6d8270c1a11cef30d9361cd424d62
Apoorva Pendse <102853901+apoorvapendse@users.noreply.github.com>
2023-12-16 23:55:41 +0530

    resolved CORS error

--- web/mongo-login/index.js
+++ web/mongo-login/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { connectDB } from './database/db.js';
 import { router } from './router/router.js';
-
+import cors from 'cors'
 const app = express();
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 app.use("/",router)
 
 app.set("view engine","ejs")
